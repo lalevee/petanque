bcac597e9309f2a2f0bd6c57fa6c230f5e669630
gitolite tester <tester@example.com>
2014-03-15 14:23:29 +0100

    Suppression jQuery... suite

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -29,15 +29,14 @@
                     <li>des <?php echo anchor('tournoi/les_resultats', 'resultats') ?> </li>
                 </ul>
 
-                <h5><font color="#0069d6">Listes</font></h5>
-                <h6><?php echo anchor('tournoi/le_reglement', 'Le règlement') ?></h6>
+                <h5><?php echo anchor('tournoi/le_reglement', 'Le règlement') ?></h5>
                 <br>
-                <h6><?php echo anchor('tournoi/les_annees_passees', 'Les années passées') ?></h6>
+                <h5><?php echo anchor('tournoi/les_annees_passees', 'Les années passées') ?></h5>
                 <br>
-                <h6><?php echo anchor('tournoi/les_credits', 'Crédits') ?></h6>
+                <h5><?php echo anchor('tournoi/les_credits', 'Crédits') ?></h5>
                 <br>
-                <h6><?php echo anchor_popup('private/admin', 'Accès organisateur') ?></h6>
+                <h5><?php echo anchor_popup('private/admin', 'Accès organisateur') ?></h5>
                 <br><br>
             </div>
-        </div>
+
 
