dd491ab45f7d46b8b7ce9d9889a49a6705f75efc
gitolite tester <tester@example.com>
2014-03-15 14:47:35 +0100

    Machine à états... suite

--- application/views/accueil.php
+++ application/views/accueil.php
@@ -3,9 +3,10 @@
         sur le <?php echo $lieu ?> !</h1>
     <br>
     <p>Ce tournoi organisé par <?php echo $association ?> est l'occasion pour
-        tous les permanents du site de participer à une activité sportive
-        conviviale permettant de mieux se connaître. La pétanque, née à La
-        Ciotat, se prête bien à cela !</p>
+        tous les permanents du <?php echo $lieu ?> de participer à une activité
+        sportive conviviale permettant de mieux se connaître. La pétanque,
+        née à La&nbsp;Ciotat, se prête bien à cela !</p>
+    
     <?php if ($etat == 'ZZZ'): ?>
         <h2>Zzzzzz....</h2>
         <p>Le site est en sommeil, juqu'à l'année prochaine...</p>
