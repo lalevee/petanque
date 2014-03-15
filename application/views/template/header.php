110b692878f63339f7ab18a4da52179852845d94
gitolite tester <tester@example.com>
2014-03-15 14:18:28 +0100

    Suppression jQuery...

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -5,20 +5,12 @@
         <title>Tournoi de pétanque</title>
         <meta name="author" content="Philippe Lalevée">
         <link rel="stylesheet" type="text/css" href="/css/bootstrap.css" />
-        <link rel="stylesheet" type="text/css" href="/css/cupertino/jquery-ui-1.10.4.custom.css" />
         <style type="text/css">
             body {
                 padding-top: 20px;
             }
         </style>
-        <link rel="shortcut icon" href="/images/favicon.ico">
-        <script type="text/javascript" src="/js/jquery-1.4.2.js"></script>
-        <script type="text/javascript" src="/js/jquery-ui-1.10.4.custom.js"></script>
-        <script>
-            $(function() {
-                $("#accordion").accordion();
-            });
-        </script>
+        <link rel="shortcut icon" href="/images/favicon.ico">   
     </head>
     <body>
 
@@ -29,28 +21,23 @@
                 <br><br>
                 <h5><?php echo anchor('tournoi', 'Accueil') ?></h5>
                 <h5><?php echo anchor('tournoi/inscription', 'Inscription') ?></h5>
-                <div id="accordion">
-                    <h5><font color="#0069d6">Listes</font></h5>
-                    <div>
-                        <ul>
-                            <li>des <?php echo anchor('tournoi/les_joueurs', 'joueurs') ?> </li>
-                            <li>des <?php echo anchor('tournoi/les_equipes', 'équipes') ?> </li>
-                            <li>des <?php echo anchor('tournoi/les_rencontres', 'matchs') ?> </li>
-                            <li>des <?php echo anchor('tournoi/les_resultats', 'resultats') ?> </li>
-                        </ul>
-                    </div>
-                    <h5><font color="#0069d6">Listes</font></h5>
-                    <div>
-                        <h6><?php echo anchor('tournoi/le_reglement', 'Le règlement') ?></h6>
-                        <br>
-                        <h6><?php echo anchor('tournoi/les_annees_passees', 'Les années passées') ?></h6>
-                        <br>
-                        <h6><?php echo anchor('tournoi/les_credits', 'Crédits') ?></h6>
-                        <br>
-                        <h6><?php echo anchor_popup('private/admin', 'Accès organisateur') ?></h6>
-                        <br><br>
-                    </div>
-                </div>
+                <h5><font color="#0069d6">Listes</font></h5>
+                <ul>
+                    <li>des <?php echo anchor('tournoi/les_joueurs', 'joueurs') ?> </li>
+                    <li>des <?php echo anchor('tournoi/les_equipes', 'équipes') ?> </li>
+                    <li>des <?php echo anchor('tournoi/les_rencontres', 'matchs') ?> </li>
+                    <li>des <?php echo anchor('tournoi/les_resultats', 'resultats') ?> </li>
+                </ul>
+
+                <h5><font color="#0069d6">Listes</font></h5>
+                <h6><?php echo anchor('tournoi/le_reglement', 'Le règlement') ?></h6>
+                <br>
+                <h6><?php echo anchor('tournoi/les_annees_passees', 'Les années passées') ?></h6>
+                <br>
+                <h6><?php echo anchor('tournoi/les_credits', 'Crédits') ?></h6>
+                <br>
+                <h6><?php echo anchor_popup('private/admin', 'Accès organisateur') ?></h6>
+                <br><br>
             </div>
         </div>
 
