3e74a90231a34c41098c70ae3bd39ebe7931c20b
Philippe Lalevée <lalevee@emse.fr>
2014-03-17 20:16:50 +0100

    formattage admin

--- application/views/private/admin.php
+++ application/views/private/admin.php
@@ -19,7 +19,7 @@
                     <li>saisie des autres informations</li>
                 </ul>
                 <li><?php echo anchor('private/admin/config', 'affichage configuration') ?></li>
-                <br>
+                <br />
             </ul>
             <li>INI -> INS: Phase d'inscription</li>
             <ul>
@@ -28,13 +28,13 @@
                     <li>passage de l'état à INS</li>
                 </ul>
                 <li><?php echo anchor('private/admin/paiement', 'paiement joueur') ?></li>
-                <br>
+                <br />
             </ul>
             <li>INS -> PRE: Préparation des équipes</li>
             <ul>
                 <li><?php echo anchor('private/admin/inscription_equipe', 'inscription équipe') ?></li>
                 <li><?php echo anchor('private/admin/tirage_equipe', 'tirage équipe') ?></li>
-                <br>
+                <br />
             </ul>
             <li>PRE -> EXP: Gestion des tours</li>
             <ul>
@@ -50,32 +50,33 @@
                     <li>génération des matchs suivants</li>
                 </ul>
             </ul>
-            <br>
+            <br />
             <li>EXP -> FIN: Tournoi terminé</li>
-            <br>
+            <br />
             <li>FIN -> ZZZ: Mise en sommeil</li>
-            <br>
-            <li>Autres fonctions</li>
-            <ul>
-                <li>envoi mail...</li>
-                <ul>
-                    <li><?php echo anchor('private/admin/mail_sgc', '... à SGC') ?></li>
-                    <li><?php echo anchor('private/admin/mail_joueurs', '... à tous les joueurs') ?></li>
-                    <li><?php echo anchor('private/admin/mail_apayer', '... aux joueurs qui doivent payer...') ?></li>
-                    <li><?php echo anchor('private/admin/mail_joueurs/TRUE', '... aux joueurs qui ont un match à jouer') ?></li>
-                    <li><?php echo anchor('private/admin/mail_equipe', '... aux joueurs d\'une équipe') ?></li>
-                </ul>
-                <li>divers...</li>
-                <ul>
-                    <li><?php echo anchor('private/admin/testmail', 'test du mail') ?></li>
-                    <li><a href="/phpmyadmin" target="_blank">phpmyadmin</a></li>
-                    <li><a href="/user_guide/" target="_blank">codeigniter user guide</a></li>
-                </ul>
-            </ul>
+            <br />
+	</ul>
+        <h2>Autres fonctions</h2>
+        <ul>
+          <li>envoi mail...</li>
+          <ul>
+            <li><?php echo anchor('private/admin/mail_sgc', '... à SGC') ?></li>
+            <li><?php echo anchor('private/admin/mail_joueurs', '... à tous les joueurs') ?></li>
+            <li><?php echo anchor('private/admin/mail_apayer', '... aux joueurs qui doivent payer...') ?></li>
+            <li><?php echo anchor('private/admin/mail_joueurs/TRUE', '... aux joueurs qui ont un match à jouer') ?></li>
+            <li><?php echo anchor('private/admin/mail_equipe', '... aux joueurs d\'une équipe') ?></li>
+          </ul>
+	  <br />
+          <li>divers...</li>
+          <ul>
+            <li><?php echo anchor('private/admin/testmail', 'test du mail') ?></li>
+            <li><a href="/phpmyadmin" target="_blank">phpmyadmin</a></li>
+            <li><a href="/user_guide/" target="_blank">codeigniter user guide</a></li>
+          </ul>
         </ul>
 
         <h2><?php echo anchor('tournoi', 'retour au site') ?></h2>
-        <br>
+        <br />
 
         <?= img('images/petanque.jpg') ?>
 
